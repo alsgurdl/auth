61a91f63ff10a9e65e219bb044900ad5f5b0b743
dudtjq <fdf98952@gmail.com>
2024-06-07 10:24:04 +0900

    5/15

--- components/Auth/AuthForm.js
+++ components/Auth/AuthForm.js
@@ -5,10 +5,10 @@ const AuthForm = () => {
   return (
     <View>
       <View>
-        <Input label='이메일 주소' />
+        <Input label='이메일 주소' keyBoardType='email-address' />
         <Input label='이름' />
-        <Input label='비밀번호' />
-        <Input label='비밀번호 확인' />
+        <Input label='비밀번호' secure='secure' />
+        <Input label='비밀번호 확인' secure />
       </View>
     </View>
   );
